8b9cf0e71ebc2a4358ff94b4e213822c7a71ed73
oribn <37297030+oribn@users.noreply.github.com>
2018-06-03 15:04:36 +0300

    Update make-it-pass.test.js

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -4,10 +4,17 @@ function sum (a, b) {
     var c;
     c=a+b;
     return c;
-  // your code to make the test pass goes here ...
 }
-
+function multiply (a, b) {
+    var c;
+    c=a*b;
+    return c;
+}
 test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.end();
+});
+test('multiply should return the multiplication of two numbers', function (t) {
+  t.equal(2, multiply(1, 2)); // make this test pass by completing the multiply function!
   t.end();
 });
